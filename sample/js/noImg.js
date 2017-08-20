bd77b9a09d40455cca97dcfee8eefd5c93e23bf8
Keillion <735699921@qq.com>
2017-08-20 09:57:52 +0800

    Create noImg.js

---
+++ sample/js/noImg.js
@@ -0,0 +1,15 @@
+var painter = new KPainter();
+painter.setOnLoading(
+	function(){ $("#grayFog").show(); },
+	function(){ $("#grayFog").hide(); }
+);
+painter.setOnNumChange(function(curIndex, length){
+	$("#pageNum").html((curIndex+1)+"/"+length);
+});
+var painterDOM = painter.getHtmlElement();
+painterDOM.style.width = '100%';
+painterDOM.style.height = '100%';
+painterDOM.style.backgroundColor = 'rgba(0,0,0,0.3)';
+$("#imgShowMdl").append(painterDOM);
+
+$("#grayFog").hide();
