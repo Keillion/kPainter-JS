7ae269806f7188a630c72b3cd2f47af4a1f59076
Keillion <735699921@qq.com>
2017-08-22 10:49:51 +0800

    call update UI when change orientation

--- sample/js/noImg.js
+++ sample/js/noImg.js
@@ -11,7 +11,7 @@ painterDOM.style.width = '100%';
 painterDOM.style.height = '100%';
 painterDOM.style.backgroundColor = 'rgba(0,0,0,0.3)';
 $("#imgShowMdl").append(painterDOM);
-$(window).resize(function(){
+$(window).on("resize orientationchange", function(){
 	painter.updateUIOnResize(true);
 });
 
